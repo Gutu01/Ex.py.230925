bb2d5dffb8bdfd170572b2870e070b44a8a9d788
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-30 15:49:41 -0300

    comentáiros

--- aprendendo/format.py
+++ aprendendo/format.py
@@ -1,3 +1,5 @@
 numero = 5
 
+# Um outro jeito de chamar uma variável
+
 print('O número é {}'.format(numero))
--- aprendendo/verifica.py
+++ aprendendo/verifica.py
@@ -1,3 +1,5 @@
+# Esses são os teste de métodos de tipo. Existe de todos os tipos para cada tipo, mas os prin
+# cipais estão aqui
 entrada = input('Digite um número: ')
 
 # Verifica se é um número, retorna verdadeiro ou falso
@@ -6,7 +8,7 @@ print('É um número:', entrada.isnumeric())
 # Verifica se é letra
 print('É letra:', entrada.isalpha())
 
-# verficar se é numero e letra
+# Verficar se é numero e letra
 print('É numero e letra:', entrada.isalnum())
 
 # Verifica se está só com letras maiusculas
