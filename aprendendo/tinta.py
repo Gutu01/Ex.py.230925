506a54c5c390aa35f8d4cf470e90919a9eba19a8
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-30 18:36:07 -0300

    Fiz um programa que calcula o tanto de tinta que vai gastar numa parede

---
+++ aprendendo/tinta.py
@@ -0,0 +1,4 @@
+altura = float(input('Digite a altura da parede: '))
+largura = float(input('Digite a largura da parede: '))
+
+print(f'Se cada litro de tinha pinta 2m², então precisará de {(altura*largura)/2} litros de tinta')
