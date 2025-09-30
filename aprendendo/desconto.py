473df3d9d1a11491138472dfc45da84aac19e019
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-30 18:41:00 -0300

    Fiz um programa que atribui desconto a um produto e outro para 15% de aumento do salário

---
+++ aprendendo/desconto.py
@@ -0,0 +1,3 @@
+preco = float(input('Digite o preço do produto: '))
+
+print(f'O novo valor dele com 5% de desconto é R${preco - (preco*5/100)}')
---
+++ aprendendo/salario.py
@@ -0,0 +1,3 @@
+salario = float(input('Insira seu salário: '))
+
+print(f'Esse é seu novo salário com 15% de aumento: R${salario + salario*15/100}')
