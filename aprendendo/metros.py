5694857f9c07624fa2b3f38818d7338b4fd2e0d4
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-30 19:01:35 -0300

    Adicionei o Km,hm,dam e dm nas medidas

--- aprendendo/metros.py
+++ aprendendo/metros.py
@@ -1,4 +1,9 @@
 metros = float(input('Digite um valor em metros: '))
 
+print('Seu valor em quilometros:', metros/1000)
+print('Seu valor em hectômetros:', metros/100)
+print('Seu valor em decâmetros:', metros/10)
+print('Seu valor em decímetros:', metros*10)
 print('Seu valor em centímetros:', metros*100)
 print('Seu valor em milímetros:', metros*1000)
+
