b803ee046a26710f997a9b25a5010f624b7ea493
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-30 17:20:25 -0300

    fiz um programa que calcula o dobro, triplo e a raiz do número

---
+++ aprendendo/dobrotriploraiz.py
@@ -0,0 +1,9 @@
+numero = int(input('Digite um número inteiro: '))
+
+dobro = numero*2
+triplo = numero*3
+raiz = numero**(1/2)
+
+print('O dobro é', dobro)
+print('O triplo é', triplo)
+print('A raiz é', raiz)
