641780d8d6d09b13ed70b508c342d2d8ae6143ea
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-09-30 18:25:11 -0300

    Fiz um programa que calcula quantos dolares você pode comprar

---
+++ aprendendo/dolar.py
@@ -0,0 +1,4 @@
+dinheiro = float(input('Digite quanto de dinheiro você tem na carteira'))
+dolar = float(input('Digite o valor do dolar hoje: '))
+
+print(f'\nVocê pode comprar U${dolar*dinheiro}')
---
+++ aprendendo/tabuadaesperta.py
@@ -0,0 +1,4 @@
+
+
+for numero in range(5):
+    print('oi')
