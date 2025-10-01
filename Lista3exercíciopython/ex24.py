201deb38b31b0b17fd10c751973d5c9c85a5b596
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-01 20:20:35 -0300

    Fiz um programa que calcula o quanto você terá que comprar em gramas de ingredientes

---
+++ Lista3exercíciopython/ex24.py
@@ -0,0 +1,9 @@
+sanduiches = int(input('Insira a quantidade de sanduíches: '))
+
+queijo = 50*2*sanduiches
+presunto = 50*sanduiches
+hamburguer = 100*sanduiches
+
+print(f'Quantidade de queijo a comprar: {queijo} gramas')
+print(f'Quantidade de presunto a comprar: {presunto} gramas')
+print(f'Quantidade de hambúrguer a comprar: {hamburguer} gramas')
