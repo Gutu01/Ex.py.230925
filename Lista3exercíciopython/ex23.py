889cb2899b00eaa3935264c718f2ee190a984478
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-01 11:28:00 -0300

    Fiz um programa que calcula a marcação de um frango

---
+++ Lista3exercíciopython/ex23.py
@@ -0,0 +1,7 @@
+frangos = int(input('Digite a quantidade de frangos se identificação:'))
+
+chip = 4
+anel_alimento = 3.50*2
+total = chip + anel_alimento
+
+print(f'Com o chip de identificação à R$2.00 e com 2 aneis de alimento por R$3.50, então ficará R${frangos*total} para marcar todos')
