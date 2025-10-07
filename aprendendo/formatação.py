69f3a2fdf2a56596fdc22e9626e68275f7b23265
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-07 01:23:38 -0300

    programa de registro aprendizado sobre fatiamento

---
+++ aprendendo/formatação.py
@@ -0,0 +1,13 @@
+frase = 'A vaca não sabe nadar'
+
+# Mostra a letra "v", pois o frase[2] vê a posição na memória onde está
+print(frase[2])
+
+# A mesma coisa para frase[2:5], mostrará "vac", não "vaca" completo
+# pois o último número vai até o anterior a ele na posição
+print(frase[2:5])
+
+print(frase[2:6])
+# Agora sim está certo   
+
+print(frase[2:20])
