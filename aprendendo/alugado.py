861a19e5f2e5a96db6619f377627ea956ce22ac2
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-08 02:11:31 -0300

    atualizei

--- aprendendo/alugado.py
+++ aprendendo/alugado.py
@@ -1,6 +1,7 @@
 km = float(input('Digite a quantidade de Km percorridos: '))
-dias = float(input('Digite a quantidade de dia que o carro foi alugado: '))
+dias = int(input('Digite a quantidade de dia que o carro foi alugado: '))
 
 total = 60*dias + 0.15*km
 
 print(f'O total a se pagar pelo carro alugado é R${total:.2f}')
+
