4c441c1eeda6aeb7c790306ccfa8566f4ca94ff7
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-08 01:57:37 -0300

    Fiz um programa que calcula o preço de um carro alugado

---
+++ aprendendo/alugado.py
@@ -0,0 +1,6 @@
+km = float(input('Digite a quantidade de Km percorridos: '))
+dias = float(input('Digite a quantidade de dia que o carro foi alugado: '))
+
+total = 60*dias + 0.15*km
+
+print(f'O total a se pagar pelo carro alugado é R${total:.2f}')
