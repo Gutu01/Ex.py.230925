fb01d1208e141da9e1ff8e410ad7c1162a93c004
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-08 00:55:51 -0300

    Fiz algumas correções

--- aprendendo/letraA.py
+++ aprendendo/letraA.py
@@ -1,8 +1,10 @@
 frase = input('Digite uma frase: ')
 quantidade_a_frase = frase.upper().replace('Ã','A').replace('Â','A').replace('Á','A').replace('À','A')
 
-# O l antes do find faz pegar o primeiro 
 print(quantidade_a_frase)
 print('\nQuantidade de vezes que a letra "a" aparece:', quantidade_a_frase.count('A'))
-print('A primeira letra "a" aparece na posição:', quantidade_a_frase.lfind('A'))
+print('A primeira letra "a" aparece na posição:', quantidade_a_frase.find('A'))
+# O r antes do find faz com que dê para pegar a posição do último intem 
+# selecionado da lista. Assim como no rstrip que tira os últimos espaços 
+# desnecessários da frase
 print('A última letra "a" aparece na posição:', quantidade_a_frase.rfind('A'))
