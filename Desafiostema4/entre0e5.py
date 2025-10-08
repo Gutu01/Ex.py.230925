983b4e017b5993076d43213475a9f0890c391fe0
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-08 17:45:39 -0400

    aprimorei o código, mas ainda preciso estudar mais para aprender

--- Desafiostema4/entre0e5.py
+++ Desafiostema4/entre0e5.py
@@ -1,6 +1,10 @@
+from random import randint
+
+computador = randint(0, 5)
+
 numero = int(input('Escolha um número entre 0 e 5: '))
 
-if numero == 0:
+if numero == computador:
     print('\nParabéns, você acertou o número que eu estava pensando!\n\n')
 else:
     print('\nVocê errou o número que eu estava pensando :(\n\n')
