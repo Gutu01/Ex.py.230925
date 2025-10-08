82208dd6cccb579f76d5590367091689c44c9b10
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-08 14:20:25 -0400

    Fiz um programa que verifica se retas formam um triângulo

---
+++ Desafiostema4/formatriangulo.py
@@ -0,0 +1,11 @@
+print('Vamos ver o que você digitar é um triângulo')
+
+reta1 = float(input('Insira o primeiro lado:'))
+reta2 = float(input('Insira o segundo lado: '))
+reta3 = float(input('Insira o terceiro lado: '))
+
+if reta1 + reta2 > reta3:
+    print('As retas formam um triângulo!\n')
+else:
+    print('As retas não formam um triângulo!\n')
+    
