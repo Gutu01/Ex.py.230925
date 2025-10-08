54f32654c787797ee9cdc02c1db764fb340b1a2d
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-08 01:25:10 -0300

    Adicionei alguns comentários

--- aprendendo/nome1.py
+++ aprendendo/nome1.py
@@ -2,5 +2,6 @@ nome = input('Digite seu nome completo:')
 nome_lista = nome.split()
 
 print('Primeiro nome:', nome_lista[0])
-# O index negativo pega o último item da lista 
+# O index negativo pega o último item da lista. O -1 pega o último, o
+# -2 pega o penúltimo e assim por diante
 print('Último nome:', nome_lista[-1])
