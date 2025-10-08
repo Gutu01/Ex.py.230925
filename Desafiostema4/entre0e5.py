5d50f3ae291a0d2dd6a7f7f77481d1cd56f89791
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-08 12:40:20 -0300

    Fiz um programa que verifica se o número digitado é o mesmo que o sistema colocou

---
+++ Desafiostema4/entre0e5.py
@@ -0,0 +1,6 @@
+numero = int(input('Escolha um número entre 0 e 5: '))
+
+if numero == 0:
+    print('\nParabéns, você acertou o número que eu estava pensando!\n\n')
+else:
+    print('\nVocê errou o número que eu estava pensando :(\n\n')
