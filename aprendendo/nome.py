86633543d17d8d737ab5299f98a6b9aa78608635
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-07 23:45:06 -0300

    Fiz um programa que lê um nome e informa coisas sobre o nome
    Coisas essas que são:
    Como ele ficaria com todas as letras minúsculas
    Como ele ficaria com todas as letras maiúsculas
    Quantas letras tem seu nome
    Quantas letras tem seu primeiro nome

--- aprendendo/nome.py
+++ aprendendo/nome.py
@@ -1,6 +1,7 @@
 nome = input('Digite seu nome: ')
-lista_nome = nome.strip()
+lista_nome = nome.split()
 
 print('Seu nome em letras minúsculas:', nome.lower())
 print('Seu nome em letras maiúsculas:', nome.upper())
-print(f'Seu nome tem {lista_nome} letras')
+print(f'Seu nome tem {len(nome.replace(' ',''))} letras')
+print(f'Seu primeiro nome tem {len(lista_nome[0])}')
