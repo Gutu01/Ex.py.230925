62dc809519943c01d0c99222e6172c5ba12f4082
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-21 17:00:52 -0400

    Atualizei meu programa para fazer ele "pensar"

--- Desafiostema4/entre0e5.py
+++ Desafiostema4/entre0e5.py
@@ -1,10 +1,18 @@
 from random import randint
+from time import sleep
 
 computador = randint(0, 5)
 
 numero = int(input('Escolha um número entre 0 e 5: '))
 
+print('\nPensando...')
+sleep(2)
+print('\n')
+print('--='*20)
 if numero == computador:
-    print('\nParabéns, você acertou o número que eu estava pensando!\n\n')
+    print('Parabéns, você acertou o número que eu estava pensando!')
 else:
-    print('\nVocê errou o número que eu estava pensando :(\n\n')
+    print('Você errou o número que eu estava pensando :(')
+print('--='*20)
+sleep(2)
+print(f'\nEu pensei no número {computador}')
---
+++ aprendendo/500porra.py
@@ -0,0 +1,7 @@
+lista = []
+
+for i in range(500+1):
+    lista.append(i)
+
+for i in range(-1, -50-1, -1):
+    print(lista[i])
