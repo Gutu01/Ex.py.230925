d941eece40eef1ee2756407d28626c51c2ac5541
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-21 22:08:10 -0300

    adicionando a lista

--- Lista3exercíciopython/ex27.py
+++ Lista3exercíciopython/ex27.py
@@ -4,16 +4,19 @@ sexo = []
 
 for i in range(1,total_alunos):
     print(f'{i}º auno\n')
-    matricula[i] = int(input('Digite sua matricula: '))
+
+    matricula.append(int(input('Digite sua matricula: ')))
+    
     print('Digite')
     print('1-Masculino')
     print('2-Feminino')
-    sexo[i] = int(input('Resposta: '))
+
+    sexo.append(int(input('Resposta: '))) 
     
     while sexo[i] > 2 or sexo[i] < 1:
         print('\nSexo inválido!\n')
         print('Digite')
         print('1-Masculino')
         print('2-Feminino')
-        sexo[i] = int(input('Resposta: '))
+        sexo.append(int(input('Resposta: '))) 
     
