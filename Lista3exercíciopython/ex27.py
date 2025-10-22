01ee520bd022d155173e60383f4072761bd01987
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-22 13:11:47 -0300

    Adicionei a altura e status

--- Lista3exercíciopython/ex27.py
+++ Lista3exercíciopython/ex27.py
@@ -1,6 +1,8 @@
 total_alunos = 3
 matricula = []
 sexo = []
+altura = []
+status = []
 
 for i in range(total_alunos):
 
@@ -21,5 +23,22 @@ for i in range(total_alunos):
         print('2-Feminino')
         sexo[i] = int(input('Resposta: '))
         
+    altura.append(float(input('Digite a altura: ')))
+
+    print("""Status físico
+          1 - Bom
+          2 - Regular
+          3 - Ruim""")
+    
+    status.append(int(input('Resposta: ')))
+
+    while status[i] > 3 or status[i] <1:
+        print("""Status não existente\n""")
+        print("""Status físico
+          1 - Bom
+          2 - Regular
+          3 - Ruim""")
+        status[i] = int(input('Resposta: '))
+    
 print(matricula,sexo)
     
