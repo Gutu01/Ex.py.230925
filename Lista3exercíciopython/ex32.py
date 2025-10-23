8665410f14fccb6aac99acfdd01b0235d8d1ddd1
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-22 22:17:36 -0300

    Acho que terminei meu programa. Vou só revisa-lo

--- Lista3exercíciopython/ex32.py
+++ Lista3exercíciopython/ex32.py
@@ -1,3 +1,4 @@
+print('\n')
 print('--='*6)
 print('Caixa registradora')
 print('--='*6)
@@ -16,33 +17,28 @@ while notas > 600 or notas < 10:
     print('--='*6)
     notas = int(input('\nDigite o valor de nota:')) 
 
-
-    notas_de_100 = notas/100
+if notas >= 100:
+    notas_de_100 = int(notas/100)
     notas = notas - 100 * notas_de_100
 
-    print(f'Notas de R$ {notas_de_100}')
-
-while notas >= 50:
-    notas_de_50 = notas/50
-    notas = notas - 50 * notas_de_50
-while notas >= 10:
-    notas_de_10 = notas/10
-    notas = notas - 10 * notas_de_10
-while notas >= 5:
-    notas_de_5 = notas/5
-    notas = notas - 5 * notas_de_5
-
-print('--='*6)
-print('Troco do dinheiro')
-print('--='*6)
-
-if notas_de_100 > 0:
     print(f'Notas de R$100: {notas_de_100}')
-if notas_de_50 > 0:
+
+if notas >= 50:
+    notas_de_50 = int(notas/50)
+    notas = notas - 50 * notas_de_50
+
     print(f'Notas de R$50: {notas_de_50}')
-if notas_de_10 > 0:
+if notas >= 10:
+    notas_de_10 = int(notas/10)
+    notas = notas - 10 * notas_de_10
     print(f'Notas de R$10: {notas_de_10}')
-if notas_de_5 > 0:
+
+if notas >= 5:
+    notas_de_5 = int(notas/5)
+    notas = notas - 5 * notas_de_5
     print(f'Notas de R$5: {notas_de_5}')
-if notas > 0:
-    print(f'Notas de R$1:')
+
+if notas >= 1:
+    print(f'Notas de R$1: {notas}')
+
+
