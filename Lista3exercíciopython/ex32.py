fd6a9ab681d43375d41d51f81c362ae0cb973d5e
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-22 21:25:05 -0300

    Estou tentando arrumar

--- Lista3exercíciopython/ex32.py
+++ Lista3exercíciopython/ex32.py
@@ -15,3 +15,34 @@ while notas > 600 or notas < 10:
     print('Valor máximo 600')
     print('--='*6)
     notas = int(input('\nDigite o valor de nota:')) 
+
+while notas >= 100:
+    notas_de_100 = notas/100
+    notas = notas - 100 * notas_de_100
+
+    print(f'Notas de R$ {notas_de_100}')
+
+while notas >= 50:
+    notas_de_50 = notas/50
+    notas = notas - 50 * notas_de_50
+while notas >= 10:
+    notas_de_10 = notas/10
+    notas = notas - 10 * notas_de_10
+while notas >= 5:
+    notas_de_5 = notas/5
+    notas = notas - 5 * notas_de_5
+
+print('--='*6)
+print('Troco do dinheiro')
+print('--='*6)
+
+if notas_de_100 > 0:
+    print(f'Notas de R$100: {notas_de_100}')
+if notas_de_50 > 0:
+    print(f'Notas de R$50: {notas_de_50}')
+if notas_de_10 > 0:
+    print(f'Notas de R$10: {notas_de_10}')
+if notas_de_5 > 0:
+    print(f'Notas de R$5: {notas_de_5}')
+if notas > 0:
+    print(f'Notas de R$1:')
