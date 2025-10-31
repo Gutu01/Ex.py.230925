8f3103beec502262d2e5b6fc6980b22464f8e46c
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-31 14:30:42 -0300

    Descobri como fazer o algoritmo que calcula o valor de água

--- Lista3exercíciopython/ex89.py
+++ Lista3exercíciopython/ex89.py
@@ -3,5 +3,35 @@ gasto = float(input('Digite o valor de m³ de água gasto: '))
 total = 37.47
 gasto = gasto - 5
 
-if gasto <= 5:
-    
+custo = [1.16, 6.46, 6.49, 6.55, 11.08]
+
+if gasto > 0:
+    if gasto > 5:
+        total += custo[0]
+    else:
+        total *= gasto
+        gasto -= gasto
+if gasto > 0:
+    if gasto > 5:
+        total += custo[1]
+    else:
+        total *= gasto
+        gasto -= gasto
+if gasto > 0:
+    if gasto > 5:
+        total += custo[2]
+    else:
+        total *= gasto
+        gasto -= gasto
+if gasto > 0:
+    if gasto > 5:
+        total += custo[3]
+    else:
+        total *= gasto
+        gasto -= gasto
+if gasto > 0:
+    if gasto > 5:
+        total += custo[4]
+    else:
+        total *= gasto
+        gasto -= gasto
