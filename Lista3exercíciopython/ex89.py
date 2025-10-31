7ca31da0060d9a11e6ac16789f8ae6a04a574da8
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-10-31 13:45:45 -0400

    Arrumei uns erros de commit

--- Lista3exercíciopython/ex89.py
+++ Lista3exercíciopython/ex89.py
@@ -1,12 +1,8 @@
 gasto = float(input('Digite o valor de m³ de água gasto: '))
 
 total = 37.47
-gasto = gasto - 5
+gasto = gasto - 5     
 
-<<<<<<< HEAD
-if gasto <= 5:
-     
-=======
 custo = [1.16, 6.46, 6.49, 6.55, 11.08]
 
 if gasto > 0:
@@ -39,4 +35,4 @@ if gasto > 0:
     else:
         total *= gasto
         gasto -= gasto
->>>>>>> 8f3103beec502262d2e5b6fc6980b22464f8e46c
+
