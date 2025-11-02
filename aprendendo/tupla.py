2c7d90efc8056a908916e99357fe4cdcdf0ede20
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-02 16:55:42 -0300

    Fiz um programa para aprender tupla

---
+++ aprendendo/tupla.py
@@ -0,0 +1,7 @@
+# Isso a baixo é um tuplia. Tuplas são imutáveis, como uma constante em c.
+# Se eu usasse um [] então seria uma lista e se fosse {} seria um dicionário
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+
+print(lanche[1])
+print(lanche[-1])
