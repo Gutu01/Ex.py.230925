b6321c61a7fb69c1a30f887d38ba28c345fc8c0b
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-01 22:55:47 -0300

    Terminei metade do exercício

--- Lista3exercíciopython/ex91.py
+++ Lista3exercíciopython/ex91.py
@@ -1,12 +1,22 @@
 notas = []
 
+soma = 0
+
 notas.append(float(input('Digite uma nota ou -1 para sair do sistema: ')))
 
 while notas[-1] != -1: 
     notas.append(float(input('Digite outra nota ou -1 para sair do sistema: ')))
 
+notas.remove(-1)
 
 print(notas)
 
-for i in range(-1):
-    print(i)
+for i in range(-1, -len(notas)-1, -1):
+    soma += notas[i]
+    print(notas[i])
+
+print(f'A soma é: {soma:.2f}')
+
+media = soma / len(notas)
+
+print(f'A média é igual a: {media:.2f}')
