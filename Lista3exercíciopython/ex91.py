958364005b0acfeb4f8401b735fb00ef9ab2f5fb
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-01 22:07:10 -0300

    arrumei umas coiass

--- Lista3exercíciopython/ex91.py
+++ Lista3exercíciopython/ex91.py
@@ -5,8 +5,8 @@ notas.append(float(input('Digite uma nota ou -1 para sair do sistema: ')))
 while notas[-1] != -1: 
     notas.append(float(input('Digite outra nota ou -1 para sair do sistema: ')))
 
-notas.remove(-1)
+
 print(notas)
 
-for i in range(-1, -1, -1):
+for i in range(-1):
     print(i)
