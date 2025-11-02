a07511348e6b999d1b0934c1eb35b5fe042b38db
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-02 16:17:51 -0300

    Terminei o programa

--- Lista3exercíciopython/ex91.py
+++ Lista3exercíciopython/ex91.py
@@ -1,6 +1,8 @@
 notas = []
 
 soma = 0
+cima_media = 0
+baixo_media = 0
 
 notas.append(float(input('Digite uma nota ou -1 para sair do sistema: ')))
 
@@ -20,3 +22,12 @@ print(f'A soma é: {soma:.2f}')
 media = soma / len(notas)
 
 print(f'A média é igual a: {media:.2f}')
+
+for i in range(0, len(notas)):
+    if notas[i] > media:
+        cima_media += 1
+    elif notas[i] < 7:
+        baixo_media += 1
+
+print(f'Valores acima da média: {cima_media}')
+print(f'Valores acima da média: {baixo_media}')
