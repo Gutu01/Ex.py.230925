893ddd166e581a349da6bbdc201832c43a62d900
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-05 01:33:14 -0400

    Fiz mais algumas alerações para aprendizagem

--- aprendendo/tupla.py
+++ aprendendo/tupla.py
@@ -8,6 +8,12 @@ print(lanche[-1])
 print(lanche[-3:])
 print(lanche[-1:-4-1:-1])
 
-
-for comida, pos in enumerate(lanche):
+# A função enumerate em for retorna dois valores, o primerio é os itens da lista e o segundo é o indice do item 
+# da lista
+for pos, comida in enumerate(lanche):
     print(f'Eu vou comer {comida} na posição {pos}')
+
+# O sorted faz a tupla aparecer em ordem
+# Ela não é mudada por si só, ela só é
+# exibida, nesse caso.
+print(sorted(lanche))
