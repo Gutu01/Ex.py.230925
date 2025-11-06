036837c62e82065baf44b327cfecade9de14bf58
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-05 21:49:11 -0400

    Fiz mais demonstrações de tupla

--- aprendendo/tupla.py
+++ aprendendo/tupla.py
@@ -15,5 +15,29 @@ for pos, comida in enumerate(lanche):
 
 # O sorted faz a tupla aparecer em ordem
 # Ela não é mudada por si só, ela só é
-# exibida, nesse caso.
+# exibida, nesse caso. Pode-se notar que 
+# a tupla também é exibida como lista 
+# por conta que tuplas é imutaveis
 print(sorted(lanche))
+
+# O mais na somas entre duas tuplas junta
+# ambas e pode ser armazenada em outra
+a = (3, 4, 6)
+b = lanche + a
+print(b)
+
+# o 4 depois da vergula fala onde tem que
+# começar a procurar, no caso ele vai
+# procurar o número 3 e mostrar o index 
+# dele na tupla.
+print(b.index(3,4))
+
+# Essa tupla é só para servir de exemplo que
+# as tuplas podem receber vários varios dados
+# de tiposs diferentes.
+pessoa = ('Gustavo', 39, 'M', 99.88)
+print(pessoa)
+
+# Isso é para mostrar que a única coisa que dá
+# pra mudar em um tupla é apagando-a.
+del(pessoa)
