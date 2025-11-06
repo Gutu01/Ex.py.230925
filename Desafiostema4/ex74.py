3384da5b620b82fb11d9335c32146667922d85f3
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 00:09:02 -0300

    Fiz um programa que, ao vocẽ digitar um valor você vê ele escrito por extenso

---
+++ Desafiostema4/ex74.py
@@ -0,0 +1,8 @@
+lista = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onde', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+numero = int(input('Digite um número inteiro entre 0 e 20: '))
+
+while numero > 20 or numero < 0:
+    numero = int(input('Valor incorreto!\nDigite um número inteiro entre 0 e 20: '))
+
+print(f'O número você escolheu foi {lista[numero]}')
