6915c03c5698a7c6829d4c59ece6831cfb92a69c
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 15:27:27 -0400

    Terminei o programa e adicionei um que procura a palavra chapecoense

--- Desafiostema4/ex75.py
+++ Desafiostema4/ex75.py
@@ -1,9 +1,12 @@
 # Usar fatiamento de tupla nesse exercício
 
-brasileirao = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Mirassol', 'Bahia', 'Botafogo', 'Fluminense', 'São paulo', 'Vasco da gama', 'Corinthians', 'atlético-MG', 'bragantino', 'grêmio', 'ceará SC', 'internacional', 'EC vitória', 'santos', 'juventude', 'fortaleza', 'sport recife')
+brasileirao = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Mirassol', 'Bahia', 'Botafogo', 'Fluminense', 'São Paulo', 'Vasco Da Gama', 'Corinthians', 'Atlético-MG', 'Bragantino', 'Grêmio', 'Ceará SC', 'Internacional', 'EC Vitória', 'Santos', 'Juventude', 'Fortaleza', 'Sport Recife')
 
 print('\n', brasileirao[:5])
 print(brasileirao[-4:])
 
 # Encontrei um problema no sorted que ele prioriza as letras maiúsculo. Então tive que colocar todos com o começo com letra maiúscula
 print(sorted(sorted(brasileirao)))
+
+if 'chapecoense' in brasileirao:
+    print(brasileirao.index('Chapecoense'))
