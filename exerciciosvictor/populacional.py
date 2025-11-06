6c085898552f50d6d4fd34861edd5b2b6882e27d
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 16:22:48 -0400

    Fiz um programa que vê quantos habitantes tem em uma cidade

---
+++ exerciciosvictor/populacional.py
@@ -0,0 +1,18 @@
+cidade_digitada = input('Digite o nome da cidade: ')
+
+cidade = cidade_digitada.upper()
+
+if cidade == 'CAMPO GRANDE':
+    print('Sua cidade tem 800 mil habitantes.')
+elif cidade == 'ANHANDUÍ':
+    print('Sua cidade tem 5 mil habitantes.')
+elif cidade == 'TERENOS':
+    print('Sua cidade tem 10 mil habitantes.')
+elif cidade == 'SIDROLÂNDIA':
+    print('Sua cidade tem 50 mil habitantes.')
+elif cidade == 'BONITO':
+    print('Sua cidade tem 100 mil habitantes.')
+elif cidade == 'COSTA RICA':
+    print('Sua cidade tem 45 mil habitantes.')
+elif cidade == 'MIRANDA':
+    print('Sua cidade tem 35 mil habitantes.')
