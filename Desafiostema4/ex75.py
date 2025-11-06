da27f9f4f2cc3d5398dbfef459d6809ff6d75288
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 16:07:50 -0400

    Fiz alguma alterações

--- Desafiostema4/ex75.py
+++ Desafiostema4/ex75.py
@@ -7,3 +7,4 @@ numero_tupla = (numeros[0], numeros[1], numeros[2], numeros[3],)
 
 print(f'O número 9 aparece {numero_tupla.count(9)} veze (s)')
 print(f'O primeiro valor 3 foi digitado na posição {numero_tupla.index(3)}')
+print('a')
