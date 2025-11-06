ebdd36efef009b49f03bdc7265302c85ef45ccc4
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 13:34:14 -0400

    Comecei a fazer um  programa que lista, em uma tupla os 20 primeiros times do brasileirão, depois vou pegar eles por fatiamento
    Add a tuple containing Brazilian football teams.

---
+++ Desafiostema4/ex75.py
@@ -0,0 +1,4 @@
+# Usar fatiamento de tupla nesse exercício
+
+brasileirao = ('palmeiras', 'flamengo', 'cruzeiro', 'mirassol', 'bahia', 'botafogo', 'fluminense', 'são paulo', 'vasco da gama', 'corinthians', 'atlético-MG', 'bragantino', 'grêmio', 'ceará SC', 'internacional', 'EC vitória', 'santos', 'juventude', 'fortaleza', 'sport recife')
+
