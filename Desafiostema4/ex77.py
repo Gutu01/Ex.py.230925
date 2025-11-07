1f5a1cd0ae1b16c0c3282c6dc1d39bb4fc829913
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 23:16:48 -0400

    Otimizei o código com a correção do Gustavo Guanabará

--- Desafiostema4/ex77.py
+++ Desafiostema4/ex77.py
@@ -1,15 +1,7 @@
 palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
 
-for i in range(len(palavras)):
-    print(f'\nNa palavra {palavras[i]} temos', end="")
-    for ii in range(len(palavras[i])):
-        if palavras[i][ii] == 'a':
-            print(' a', end="")
-        elif palavras[i][ii] == 'e':
-            print(' e', end="")
-        elif palavras[i][ii] == 'i':
-            print(' i', end="")
-        elif palavras[i][ii] == 'o':
-            print(' o', end="")
-        elif palavras[i][ii] == 'u':
-            print(' u', end="")
+for i in palavras:
+    print(f'\nNa palavra {i} temos', end=" ")
+    for ii in i:
+        if ii in 'aeiou':
+            print(ii, end=" ")
