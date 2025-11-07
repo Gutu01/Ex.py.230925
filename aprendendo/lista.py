62a7a069d0f7091ebaefc00a1fea6a23af011b84
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-07 01:15:17 -0300

    Adicionei mais algumas funções para consultar depois

--- aprendendo/lista.py
+++ aprendendo/lista.py
@@ -10,13 +10,34 @@ valores= list(range(4, 11))
 
 print(valores)
 
-valores2 = [8, 4, 3, 2, 1, 9, 3 ,4]
+valores = [8, 4, 3, 2, 1, 9, 3 ,4]
 # O sort deixa a lista em ordem
-valores2.sort()
+valores.sort()
 
-print(valores2)
+print(valores)
 
 # O reverse deixa a lista ordenada de forma inversa
-valores2.sort(reverse=True)
+valores.sort(reverse=True)
 
-print(valores2)
+print(valores)
+
+# Essa função vai adicionar o valor 69 na posição 3 e empurrar os demais
+#para frente
+valores.insert(3, 69) 
+
+print(valores)
+
+# Essa função, sem parametro, remove o último valor
+valores.pop()
+
+print(valores)
+
+# A opção "a" não está fazendo uma cópia de valores, ela está fazendo
+#uma ligação, logo, se eu mudar algo em valores, irá mudar em a também.
+# Diferentemente de "b" que fazer uma cópia da lista inteira
+a = valores
+b = valores[:]
+
+valores.append(6969)
+print(a)
+print(b)
