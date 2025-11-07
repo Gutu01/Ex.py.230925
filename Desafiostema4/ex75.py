728ed08bd9d0c92b541ae0a1f0fdc8e43e41ae34
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 21:02:52 -0400

    Fiz um else caso não for encontrado o número 3

--- Desafiostema4/ex75.py
+++ Desafiostema4/ex75.py
@@ -8,8 +8,10 @@ for i in range(4):
 numero_tupla = (numeros[0], numeros[1], numeros[2], numeros[3],)
 
 print(f'O número 9 aparece {numero_tupla.count(9)} veze (s)')
-print(f'O primeiro valor 3 foi digitado na posição {numero_tupla.index(3)}')
-
+if 3 in numero_tupla:
+    print(f'O primeiro valor 3 foi digitado na posição {numero_tupla.index(3)}')
+else:
+    print('Não foi encontrado nenhum número 3')
 for i in range(4):
     if numeros[i] % 2 == 0:
         par += 1
