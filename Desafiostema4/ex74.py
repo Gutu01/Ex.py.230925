f3c7dc4c7fac47adede047580421eb0dcfe1d098
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-06 20:55:08 -0400

    Fiz um programa que gera 5 números aleatórios e depois lista eles e ainda diz qual o maior e qual é o menor

--- Desafiostema4/ex74.py
+++ Desafiostema4/ex74.py
@@ -1,4 +1,13 @@
 from random import randint
 
-teste = randint(0,10)
+numero1 = randint(0,10)
+numero2 = randint(0,10)
+numero3 = randint(0,10)
+numero4 = randint(0,10)
+numero5 = randint(0,10)
 
+numeros = (numero1, numero2, numero3, numero4, numero5)
+
+print(numeros)
+print(f'O maior valor é {max(numeros)}')
+print(f'O menor valor é {min(numeros)}')
