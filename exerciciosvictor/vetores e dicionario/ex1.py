dc9941734b9f28ae3d72cf6ee72d78699b2c9913
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-19 14:21:57 -0400

    Adicionei a função de cadastrar livro
    acho que ainda não terminei

--- exerciciosvictor/vetores e dicionario/ex1.py
+++ exerciciosvictor/vetores e dicionario/ex1.py
@@ -1,9 +1,14 @@
-livros = []
+nome = []
+autor = []
+genero = []
+ano = []
+status = []
+
 escolha = 1
 
 while escolha:
 
-    print('1 - Cadastrar livro')
+    print('\n1 - Cadastrar livro')
     print('2 - Buscar por nome')
     print('3 - Bucar por autor')
     print('4 - Buscar por gênero')
@@ -15,4 +20,21 @@ while escolha:
 
     match escolha:
         case 1:
-            livros.append(input('Digite o nome do '))
+            nome.append(input('\nDigite o nome do livro:'))
+            autor.append(input('Digite o autor do livro: '))
+            genero.append(input('Digite o gênero do livro: '))
+            ano.append(input('Digite o ano que foi lançado o livro: '))
+
+        case 2:
+            print('\nDigite a escolha de busca')
+            print('1 - Nome')
+            print('2 - Autor')
+            print('3 - Gênero')
+            busca = int(input('Resposta:'))
+
+            while busca != 1 and busca != 2 and busca != 3:
+                print('\nDigite a escolha de busca')
+                print('1 - Nome')
+                print('2 - Autor')
+                print('3 - Gênero')
+                busca = input('Resposta:')  
