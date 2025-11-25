56d928e89a2f574d87e4509c3c1bfc6abd568c76
Gustavo Martins Villa <gustavomt058@gmail.com>
2025-11-24 20:01:52 -0400

    Fiz um programa que cria uma lista normal e outras duas dizendo se é par ou impar

---
+++ Desafiostema4/ex82.py
@@ -0,0 +1,17 @@
+lista = []
+par = []
+impar = []
+
+numero = 1
+
+while numero:
+    numero = (int(input('Digite um número inteiro ou 0 para sair')))
+    lista.append(numero)
+
+    if numero % 2 == 0:
+        par.append(numero)
+    else:
+        impar.append(numero)
+
+print(f'A lista original {lista}')
+print(f'A lista de pares')
