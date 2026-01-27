991bac9f0d1c225184c620db458249cd3d3cd312
Gustavo Martins Villa <gustavomt058@gmail.com>
2026-01-27 15:41:27 -0400

    Fiz a 5

--- exerciciosvictor/listamatrix/5.py
+++ exerciciosvictor/listamatrix/5.py
@@ -9,3 +9,13 @@ B = [
     [13, 14, 15],
     [16, 17, 18]
     ]
+
+C = []
+
+for i in range(3):
+    l = []
+    for p in range(3):
+        l.append(A[i][p] + B[i][p])
+    C.append(l)
+
+print(f'\n{C[0]}\n{C[1]}\n{C[2]}')
