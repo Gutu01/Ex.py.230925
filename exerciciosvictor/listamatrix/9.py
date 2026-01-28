54a128a0a4cfb6301f01bf936c0ba5dc3f362788
Gustavo Martins Villa <gustavomt058@gmail.com>
2026-01-27 20:06:34 -0400

    Fiz mais um código

---
+++ exerciciosvictor/listamatrix/9.py
@@ -0,0 +1,24 @@
+A = [
+    [1, 2, 3],
+    [4, 5, 6]
+    ]
+
+B = [
+    [7, 8],
+    [9, 10],
+    [11, 12],
+    ]
+
+multiplicacao = []
+
+for i in range(len(A)):
+    l = []
+    for p in range(len(A)):
+        soma = 0
+        for j in range(len(B)):
+            soma += A[i][j] * B[j][p]
+        l.append(soma)
+    multiplicacao.append(l)
+
+for i in range(2):
+    print(f'{multiplicacao[i]}')
